506a04d3960e2ffa4f2fe873fa95d576174398a5
droidinspector <matveitoro@gmail.com>
2022-06-16 23:36:25 +0300

    add content to plans

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,9 +1,57 @@
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import './Plans.css';
 
 function Plans() {
   return (
     <section className="section-plans">
-      Plans
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2026"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            AUCA, Software Engineering, Bachelor Degree
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2028"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            MIT, Software Engineering, Master Degree
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2033"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Google, Director Software Development
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2043"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Founder and CEO of own IT company
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2048"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Self Made Multi Billionaire
+          </h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </section>
   );
 }
