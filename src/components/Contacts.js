67fac6846ace95ba20b79153d1e5323534edcb2a
droidinspector <matveitoro@gmail.com>
2022-06-16 23:48:13 +0300

    add content to contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,7 +3,9 @@ import './Contacts.css';
 function Contacts() {
   return (
     <section className="section-contacts">
-      Contacts
+      <h1>Reach out to me</h1>
+      <a href="mailto:emir.torokulov@gmail.com"><h2>emir.torokulov@gmail.com</h2></a>
+      <h3>Have a good day!</h3>
     </section>
   );
 }
