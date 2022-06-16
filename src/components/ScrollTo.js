e3561089a2cb37f9c1fc3ecc1e8801c5b4fd56ce
droidinspector <matveitoro@gmail.com>
2022-06-17 00:05:11 +0300

    add build

--- src/components/ScrollTo.js
+++
@@ -1,9 +0,0 @@
-import {scrollTo} from '../utils/scroll';
-
-const ScrollTo = ({ toId, toRef, duration = 1000, children }) => {
-  const handleClick = () => scrollTo({ id: toId, ref: toRef, duration });
-  
-  return <button onClick={handleClick}>{children}</button>;
-};
-
-export default ScrollTo;
--- src/components/Section.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Section = React.forwardRef(({ id, children }, ref) => (
-  <section className="section" ref={ref} id={id}>
-    {children}
-  </section>
-));
-
-export default Section;
