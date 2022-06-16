34662a9e2ba09a2a9486271c48668feb38482fda
droidinspector <matveitoro@gmail.com>
2022-06-16 23:19:01 +0300

    add content to hobbies

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -10,9 +10,6 @@ function Greeting() {
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lorem metus, laoreet vitae libero in, suscipit viverra ligula. Sed iaculis ligula eget rutrum scelerisque. Nullam orci orci, dictum et orci vel, aliquet varius dolor. Suspendisse nec varius velit. Morbi eget tincidunt felis. Donec a lacus sed orci laoreet lobortis. Praesent gravida id erat placerat commodo. Curabitur erat sapien, dapibus ac vestibulum nec, hendrerit id leo.
         </p>
-        <p>
-          Aliquam erat volutpat. Aenean massa tortor, varius non ex vel, dignissim dignissim metus. Donec lobortis consequat congue. Nunc condimentum bibendum tincidunt. Sed rhoncus aliquet lectus in iaculis. Nullam vitae dignissim enim. Pellentesque pretium placerat nisi at maximus. Nulla et tellus sed orci blandit ornare vitae vel ipsum. Vestibulum varius felis ac maximus rhoncus. Aenean lacinia, nisi et mattis eleifend, eros lorem pulvinar dui, semper porttitor est quam quis purus. Curabitur sed nibh magna. Curabitur lectus turpis, mollis eu libero a, consequat cursus nulla.
-        </p>
         <span>Use navigation bar for smooth transitions ⬆⬆⬆</span>
       </div>
       <img src={WaveImg} alt="hello" className="waving-hand" />
--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,9 +1,23 @@
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 import './Hobbies.css';
+
+const images = [
+  {
+    original: 'https://sbooks.net/wp-content/uploads/2021/10/old-book-flying-letters-magic-light-background-bookshelf-library-ancient-books-as-symbol-knowledge-history-218640948.jpg',
+  },
+  {
+    original: 'https://cutewallpaper.org/23/anime-compilation-wallpaper/95632834.jpg',
+  },
+  {
+    original: 'https://wallpaperaccess.com/full/2256612.jpg',
+  },
+];
 
 function Hobbies() {
   return (
     <section className="section-hobbies">
-      Hobbies
+      <ImageGallery items={images} />;
     </section>
   );
 }
